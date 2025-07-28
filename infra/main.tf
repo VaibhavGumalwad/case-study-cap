e03b75b507f9cd19f74e2dc22366d6560f1bb2a9
Prathamesh Borade <pborade90@gmail.com>
2025-07-28 14:14:03 +0530

    Update Jenkinsfile

--- infra/main.tf
+++ infra/main.tf
@@ -100,7 +100,7 @@ resource "aws_instance" "app" {
   instance_type               = var.instance_type
   subnet_id                   = aws_subnet.public.id
   vpc_security_group_ids      = [aws_security_group.sg.id]
-  associate_public_ip_address = false
+  associate_public_ip_address = true
   key_name                    = "my-ec2-deploy-key"
 
   tags = {
